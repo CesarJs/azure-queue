6c3437de133be4f9be645a4cdec25e26032d0847
Cesar Augusto <cesar.lima@etapa.com.br>
2020-12-08 15:42:06 -0300

    Adicionando fila

--- src/QueueAzure.php
+++ src/QueueAzure.php
@@ -2,10 +2,34 @@
 
 namespace Etapa\QueueAzure;
 
+use MicrosoftAzure\Storage\Queue\QueueRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Queue\Models\CreateQueueOptions;
+
 class QueueAzure
 {
-    public function greet(String $sName = 'teste')
-    {
-        return 'Hi ' . $sName . '! How are you doing today?';
+    public function sendQueue($data){
+        if(!$data || $data == null){
+            throw new \Exception("Error Processing Request", 1);
+        }
+
+        $connectionString = env('CONNECTION_STRING', '');
+        $queueClient = QueueRestProxy::createQueueService($connectionString);
+        $createQueueOptions = new CreateQueueOptions();
+
+        foreach ($data as $key => $value) {
+            $createQueueOptions->addMetaData($key, $value);
+        }
+
+        try    {
+            $queueClient->createQueue($data['module'], $createQueueOptions);
+        }
+        catch(ServiceException $e){
+
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            echo $code.": ".$error_message."<br />";
+        }
     }
+
 }
